d0788951febc6cddeb2aae876904ff63ead70ff0
Markus <nummimarkus@gmail.com>
2022-01-30 19:37:48 +0200

    holding the block and I guess there was one bug also somewhere..

---
+++ Tetriss/GameState.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    class GameState
+    {
+        private Block currentBlock;
+
+        public Block CurrentBlock {
+
+            get => currentBlock;
+            private set {
+                currentBlock = value;
+                currentBlock.Reset();
+
+                for (int i = 0; i < 2; i++) {
+                    //FOR BETTER BLOCK SPAWNING
+                    currentBlock.Move(1, 0);
+
+                    if (!BlockFits()) {
+
+                        currentBlock.Move(-1, 0);
+                    }
+                }
+
+            }
+
+
+        }
+        public GameGrid GameGrid { get; }
+        public BlockQueue BlockQueue { get; }
+        public bool GameOver { get; private set; }
+
+        public int score { get; private set; }
+
+        public Block HeldBlock { get; private set; } 
+        public bool CanHold { get; private set; }
+        public GameState() {
+
+            GameGrid = new GameGrid(22, 10);
+            BlockQueue = new BlockQueue();
+            CurrentBlock = BlockQueue.GetAndUpdate();
+            CanHold = true;
+        }
+        private bool BlockFits() {
+            //Loop over positions of currentblock, if any of them is outside the grid or overlapping with other tile, we return false. Otherwise we return true.
+            foreach (Position p in CurrentBlock.TilePositions()) {
+
+                if (!GameGrid.IsEmpty(p.Row, p.Column)) { 
+                    return false; 
+                }
+            
+            }
+
+            return true;
+        }
+        public void HoldBlock() {
+
+            if (!CanHold) {
+                return;
+            }
+            if (HeldBlock == null)
+            {
+                HeldBlock = CurrentBlock;
+                CurrentBlock = BlockQueue.GetAndUpdate();
+            }
+            else { //swapping the heldblock
+                Block tmp = CurrentBlock;
+                CurrentBlock = HeldBlock;
+                HeldBlock = tmp;
+            }
+            //So we cant just spam the hold
+            CanHold = false;
+        }
+        public void RotateBlockCW() {
+
+            CurrentBlock.RotateCW();
+            if (!BlockFits()) { 
+            CurrentBlock.RotateCCW();
+            }
+
+        }
+        public void RotateBlockCCW()
+        {
+
+            CurrentBlock.RotateCCW();
+            if (!BlockFits())
+            {
+                CurrentBlock.RotateCW();
+            }
+
+        }
+        public void MoveBlockLeft() {
+
+            CurrentBlock.Move(0, -1);
+            if (!BlockFits()) {
+                MoveBlockRight();
+            }
+        }
+        public void MoveBlockRight() {
+
+            CurrentBlock.Move(0, 1);
+            if (!BlockFits())
+            {
+                MoveBlockLeft();
+            }
+        }
+        private bool IsGameOver() { 
+        
+            return !(GameGrid.IsRowEmpty(0) && GameGrid.IsRowEmpty(1));
+        }
+        private void PlaceBlock() {
+
+            foreach (Position p in CurrentBlock.TilePositions()) {
+
+                GameGrid[p.Row, p.Column] = CurrentBlock.Id;
+            }
+            score += GameGrid.ClearFullRows();
+
+            if (IsGameOver())
+            {
+                GameOver = true;
+            }
+            else {
+                CurrentBlock = BlockQueue.GetAndUpdate();
+                CanHold = true;
+            }
+
+
+        }
+        public void MoveBlockDown() { 
+            CurrentBlock.Move(1,0);
+
+            if (!BlockFits()) { 
+            
+            CurrentBlock.Move(-1,0);
+                PlaceBlock();
+            }
+
+        }
+        public void MoveBlockDownFast()
+        {
+
+
+            for (int i = 0; i < 22; i++) {
+
+
+                CurrentBlock.Move(1, 0);
+                if (!BlockFits())
+                {
+
+                    CurrentBlock.Move(-1, 0);
+                    PlaceBlock();
+                    break;
+                }
+            }
+            
+        }
+
+
+    }
+}
--- Tetriss/MainWindow.xaml.cs
+++ Tetriss/MainWindow.xaml.cs
@@ -1,17 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Tetris;
 
 namespace Tetriss
 {
@@ -20,9 +14,158 @@ namespace Tetriss
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private readonly ImageSource[] tileImages = new ImageSource[] {
+
+
+            new BitmapImage(new Uri("Assets/TileEmpty.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/TileCyan.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/TileBlue.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/TileOrange.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/TileYellow.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/TileGreen.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/TilePurple.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/TileRed.png", UriKind.Relative))
+
+        };
+        private readonly ImageSource[] blockImages = new ImageSource[] {
+
+            new BitmapImage(new Uri("Assets/Block-Empty.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/Block-I.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/Block-J.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/Block-L.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/Block-O.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/Block-S.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/Block-T.png", UriKind.Relative)),
+            new BitmapImage(new Uri("Assets/Block-Z.png", UriKind.Relative))
+
+        };
+        private readonly Image[,] imageControls;
+        private GameState gameState = new GameState();
         public MainWindow()
         {
             InitializeComponent();
+            imageControls = SetupGameCanvas(gameState.GameGrid);
+        }
+
+        private Image[,] SetupGameCanvas(GameGrid gameGrid) {
+
+            Image[,] imageControls = new Image[gameGrid.Rows, gameGrid.Columns];
+            int cellSize = 25;
+            for (int row = 0; row < gameGrid.Rows; row++) { 
+            
+                   for(int col = 0; col < gameGrid.Columns; col++)
+                {
+                    Image imageControl = new Image
+                    {
+                        Width = cellSize,
+                        Height = cellSize
+
+                    };
+                Canvas.SetTop(imageControl, (row - 2) * cellSize + 10);
+                Canvas.SetLeft(imageControl, col * cellSize);
+                    GameCanvas.Children.Add(imageControl);
+                    imageControls[row,col] = imageControl;
+            }
+            }
+            return imageControls;
+        }
+        private void DrawGrid(GameGrid gameGrid)
+        {
+
+            for (int row = 0; row < gameGrid.Rows; row++)
+            {
+
+                for (int col = 0; col < gameGrid.Columns; col++)
+                {
+                    int id = gameGrid[row, col];
+                    imageControls[row, col].Source = tileImages[id];
+
+                }
+
+            }
+        }
+        private void DrawBlock(Block block) {
+
+            foreach (Position p in block.TilePositions())
+            {
+                
+                imageControls[p.Row, p.Column].Source = tileImages[block.Id];
+                
+            }
+
+        }
+
+        private void DrawNextBlock(BlockQueue blockQueue) {
+
+            Block next = blockQueue.NextBlock;
+            NextImage.Source = blockImages[next.Id];        
+        }
+        private void DrawHoldBlock() { 
+        
+        }
+
+        private void Draw(GameState gameState) {
+            DrawGrid(gameState.GameGrid);
+            DrawBlock(gameState.CurrentBlock);
+            DrawNextBlock(gameState.BlockQueue);
+            ScoreText.Text = "Score: " + gameState.score.ToString();
+        }
+        private async Task GameLoop() {
+
+            Draw(gameState);
+            while (!gameState.GameOver) {
+
+                await Task.Delay(500);
+                gameState.MoveBlockDown();
+                Draw(gameState);
+            }
+            GameOverMenu.Visibility = Visibility.Visible;
+            FinalScoreText.Text = "Score: " + gameState.score.ToString();
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (gameState.GameOver) { return; }
+
+            switch (e.Key) {
+
+                case Key.Left:
+                    gameState.MoveBlockLeft();
+                    break;
+                case Key.Right:
+                    gameState.MoveBlockRight();
+                    break;
+                case Key.Down:
+                    gameState.MoveBlockDown();
+                    break;
+                case Key.Up:
+                    gameState.RotateBlockCW();
+                    break;
+                case Key.Z:
+                    gameState.RotateBlockCCW();
+                    break;
+                case Key.Space:
+                    gameState.MoveBlockDownFast();
+                    break;
+                case Key.C:
+                    gameState.HoldBlock();
+                    break;
+
+            }
+            Draw(gameState);
+        }
+
+        private async void GameCanvas_Loaded(object sender, RoutedEventArgs e)
+        {
+            await GameLoop();
+        }
+
+        private async void Play_Again_Click(object sender, RoutedEventArgs e)
+        {
+            gameState = new GameState();
+            GameOverMenu.Visibility = Visibility.Hidden;
+            await GameLoop();
         }
     }
 }
